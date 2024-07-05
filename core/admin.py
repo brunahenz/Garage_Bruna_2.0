6aad661d909106d0b0807bf7d2038113417b50bd
brunahenz <brunaehenz@gmail.com>
2024-07-05 15:12:57 -0300

    marcas, okay

--- core/admin.py
+++ core/admin.py
@@ -53,4 +53,6 @@ admin.site.register(models.User, UserAdmin)
 admin.site.register(models.Acessorio)
 admin.site.register(models.Categoria)
 admin.site.register(models.Cor)
+admin.site.register(models.Marca)
+
 
